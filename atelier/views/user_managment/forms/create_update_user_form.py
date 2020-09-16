23aef4b7c355b693eb6b7f49b201910a82df7968
vitamal <vitamal@ukr.net>
2020-09-16 17:01:40 +0300

    #7 still working with html user management

--- atelier/views/user_managment/forms/create_update_user_form.py
+++ atelier/views/user_managment/forms/create_update_user_form.py
@@ -9,7 +9,6 @@ from ...view_mixins import RedirectSuccessFormMixin
 
 
 class UserUpdateForm(PasswordForm):
-
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'occupation', 'is_administrator', 'password1', 'password2']
@@ -25,13 +24,13 @@ class UserCreateForm(UserCreationForm, RedirectSuccessFormMixin):
         required=True,
         label=_('Password'),
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control', 'required': True}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', 'class': 'form-control text-white', 'required': True}),
         help_text=password_validation.password_validators_help_text_html(),
     )
 
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'required': True, 'autocomplete': 'new-password'}),
+        widget=forms.PasswordInput(attrs={'class': 'form-control text-white', 'required': True, 'autocomplete': 'new-password', 'id': 'materialRegisterFormPassword'}),
         strip=False,
         help_text=_('Enter the same password as before, for verification.'),
         label=_('Password confirmation'),
@@ -42,8 +41,10 @@ class UserCreateForm(UserCreationForm, RedirectSuccessFormMixin):
         fields = RedirectSuccessFormMixin.Meta.fields + [
             'username', 'first_name', 'last_name', 'email', 'is_administrator', 'occupation', 'password1', 'password2']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'required': True}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
+            'username': forms.TextInput(attrs={'class': 'form-control text-white', 'required': True}),
+            'email': forms.EmailInput(attrs={'class': 'form-control text-white', 'id': 'materialRegisterFormEmail', 'required': True}),
+            'first_name': forms.TextInput(
+                attrs={'class': 'form-control text-white', 'id': 'materialRegisterFormFirstName', 'required': True}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control text-white', 'id': 'materialRegisterFormLastName', 'required': True}),
+            'is_administrator': forms.CheckboxInput(attrs={'class': 'checkbox'}),
         }
